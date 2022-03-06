903c94b232b893adf4c36adc862164f56a8cb211
Marcel Maatkamp <m.maatkamp@gmail.com>
2022-03-06 17:20:11 +0100

    argo

--- etc/terraform/modules/argocd/main.tf
+++ etc/terraform/modules/argocd/main.tf
@@ -1,19 +1,7 @@
-resource "kubernetes_namespace" "argocd" {
-  metadata {
-    annotations = {
-      name = "argocd"
-    }
-
-    labels = {
-      mylabel = "argocd"
-    }
-    name = "argocd"
-  }
-}
-
 resource "helm_release" "argocd" {
   name       = "argocd" 
   namespace  = "argocd"
+  create_namespace = true
 
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
--- main.tf
+++ main.tf
@@ -33,35 +33,6 @@ module "argocd" {
   ]
 }
 
-resource "argocd_project_token" "secret" {
-  project      = "someproject"
-  role         = "foobar"
-  description  = "short lived token"
-  expires_in   = "1h"
-  renew_before = "30m"
-}
-
-data "kubernetes_secret" "argocd_admin_password" {
-  metadata { 
-    namespace = "argocd"
-    name = "argocd-secret"
-  }
-  binary_data = {
-    "admin.password" = ""
-  }
-  depends_on = [
-    module.argocd
-  ]
-}
-
-provider "argocd" {
-# namespace = "argocd"
-  server_addr = "argocd-server:443"
-# auth_token  = resource.argocd_project_token.secret.jwt
-  username = "admin"
-  password = data.kubernetes_secret.argocd_admin_password.binary_data["admin.password"]
-}
-
 module "grafana" {
   source = "./etc/terraform/modules/grafana"
   config_path = local.config_path
