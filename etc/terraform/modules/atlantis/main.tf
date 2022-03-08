5327892658568e401b60146a6754db40cbc7268e
Marcel Maatkamp <m.maatkamp@gmail.com>
2022-03-08 19:38:01 +0100

    new files

---
+++ etc/terraform/modules/atlantis/main.tf
@@ -0,0 +1,29 @@
+resource "argocd_application" "helm" {
+  metadata {
+    name      = "atlantis"
+    namespace = "atlantis"
+  }
+
+  wait = true
+
+  spec {
+    source {
+# runatlantis/atlantis	3.16.0       	v0.19.1    	A Helm chart for Atlantis https://www.runatlant...
+      repo_url        = "https://runatlantis.github.io/helm-charts"
+      chart           = "atlantis"
+      target_revision = "3.16.0"
+      helm {
+        value_files = ["values-test.yml"]
+        values      = <<EOT
+atlantis:
+  atlantisUrl: "https://x.y.z"
+EOT
+      }
+    }
+
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = "default"
+    }
+  }
+}
